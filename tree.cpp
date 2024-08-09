5d043739f0d94e12872a5ded32bcdeb0455bfe88
HP_3500 <sergeymaksimov78@yandex.ru>
2024-08-10 01:13:55 +0700

    check tree

--- tree.cpp
+++ tree.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <string>
 #include "s21_tree.h"
 
 int main() {
-  s21::Tree<int, int> dubik;
-  s21::Tree<int, int> baobab;
+  s21::Tree<int, int> dubik(std::pair(3,9));
+  s21::Tree<int, char> baobab(std::pair(11, 'A'));
+  s21::Tree<int, std::string> iva(std::pair(333, "veter"));
+  s21::Tree<int, int> elj;
+  std::pair<int, char> pair_1(76, 'B');
   std::cout << "dubik, baobab" << std::endl;
+  std::cout << "check empty dubik   " << dubik.empty() << std::endl;
+  std::cout << "check empty elj     " << elj.empty() << std::endl;
+  std::cout << "check size               " << baobab.size() << std::endl;
+  // std::cout << "begin   " << iva.begin() << std::endl;
+
+
+  return 0;
 }
