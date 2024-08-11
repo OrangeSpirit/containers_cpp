8f185cf3743737f82d033bd7bcea3a88e08f5273
serg <sergeymaksimov@yandex.ru>
2024-08-11 07:19:00 +0700

    modified tree

--- set.cpp
+++ set.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <initializer_list>
+#include <set>
 #include "s21_tree.h"
 
 namespace s21 {
-  template <typename T>
+  template <typename Key>
   class set {
     public:
     using key_type = Key; // the first template parameter (Key)
@@ -14,34 +15,37 @@ namespace s21 {
     //using const_iterator = internal class SetConstIterator<T> or BinaryTree::const_iterator as the internal const iterator of tree subclass; defines the constant type for iterating through the container
     using size_type = size_t; // defines the type of the container size (standard type is size_t)
 
-    set();
-    set(std::initializer_list<value_type> const &items);
+    set(){};
+    set(std::initializer_list<Key> &items) {
+      for (value_type value : items) Tree<Key, Key>::insert(value);
+    };
     set(const set &s);
     set(set &&s);
     ~set();
     set &operator=(set &&s);
 
-    iterator begin(); //  returns an iterator to the beginning
-    iterator end(); //  returns an iterator to the end
+    // iterator begin(); //  returns an iterator to the beginning
+    // iterator end(); //  returns an iterator to the end
 
-    bool empty(); //  checks whether the container is empty
-    size_type size(); //  returns the number of elements
-    size_type max_size(); //  returns the maximum possible number of elements
+    // bool empty(); //  checks whether the container is empty
+    // size_type size(); //  returns the number of elements
+    // size_type max_size(); //  returns the maximum possible number of elements
 
-    void clear(); //  clears the contents
-    std::pair<iterator, bool> insert(const value_type& value);  //  inserts node and returns iterator to where the element is in the container and bool denoting whether the insertion took place
-    void erase(iterator pos); //  erases element at pos
-    void swap(set& other);  //  swaps the contents
-    void merge(set& other); //  splices nodes from another container
+    // void clear(); //  clears the contents
+    // std::pair<iterator, bool> insert(const value_type& value);  //  inserts node and returns iterator to where the element is in the container and bool denoting whether the insertion took place
+    // void erase(iterator pos); //  erases element at pos
+    // void swap(set& other);  //  swaps the contents
+    // void merge(set& other); //  splices nodes from another container
 
-    iterator find(const Key& key);  //  finds element with specific key
-    bool contains(const Key& key);  //  checks if the container contains element with specific key
-  }
-};
+    // iterator find(const Key& key);  //  finds element with specific key
+    // bool contains(const Key& key);  //  checks if the container contains element with specific key
+  };
+} // namespace s21
 
 int main() {
-int a = 111;
-std::cout << "test set = " << a << std::endl;
+  s21::set<int> test_set = {1, 2, 3, 4, 5};
+  std::set<int> orig_set = {1, 2, 3, 4, 5};
+
 
 
   return 0;
--- tree.cpp
+++ tree.cpp
@@ -8,10 +8,21 @@ int main() {
   s21::Tree<int, std::string> iva(std::pair(333, "veter"));
   s21::Tree<int, int> elj;
   std::pair<int, char> pair_1(76, 'B');
+
+  std::pair<int, int> p1(1, 1);
+  std::pair<int, int> p2(3, 3);
+  std::pair<int, int> p3(11, 11);
+  std::pair<int, int> p4(22, 22);
+  std::pair<int, int> p5(77, 77);
+
+  s21::Tree<int, int> big_tree{p1, p2, p5, p4, p3};
+  std::cout << "big_tree size =   " << big_tree.size() << std::endl;
   std::cout << "dubik, baobab" << std::endl;
   std::cout << "check empty dubik   " << dubik.empty() << std::endl;
   std::cout << "check empty elj     " << elj.empty() << std::endl;
   std::cout << "check size               " << baobab.size() << std::endl;
+
+  std::cout << "big_tree person" << big_tree.GetNode_() << std::endl;
   // std::cout << "begin   " << iva.begin() << std::endl;
 
 
